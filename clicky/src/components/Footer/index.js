aabde0826028bb0ac3aff96dfde52f6a2e11b816
JHaarla <haarladesign@gmail.com>
2019-12-21 16:38:34 -0500

    Updates

--- clicky/src/components/Footer/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Footer.css";
-
-const Footer = () => <div className="footer text-center" id="footer">© 2019 Jarkko Haarla</div>
-
-
-export default Footer;
---
+++ precache-manifest.d5e9f98228be7761b90616ceac1b6116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149f6dc8a1904e5f47628c3c778b4566",
+    "url": "/game-of-clicks/index.html"
+  },
+  {
+    "revision": "90405553776822b24310",
+    "url": "/game-of-clicks/static/css/main.95a3e2ed.chunk.css"
+  },
+  {
+    "revision": "950f4f7890df28b4d4de",
+    "url": "/game-of-clicks/static/js/2.b2383e26.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-of-clicks/static/js/2.b2383e26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90405553776822b24310",
+    "url": "/game-of-clicks/static/js/main.e8f3e170.chunk.js"
+  },
+  {
+    "revision": "593a9eb09a64810ffcf1",
+    "url": "/game-of-clicks/static/js/runtime-main.b732e12e.js"
+  }
+]);
